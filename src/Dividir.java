802296b3830229821c3a955af449b7189d6d43fe
Vladimir-28 <20243ds028@utez.edu.mx>
2025-09-02 11:45:52 -0600

    Creacion de metodos para encapsulamiento, obtener resultado y validar los datos

--- src/Dividir.java
+++ src/Dividir.java
@@ -1,5 +1,44 @@
 public class Dividir {
     private double num1;
     private double num2;
-    private double result;
+
+    public Dividir(double num1, double num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public Dividir(){
+
+    }
+
+    public double getNum1() {
+        return num1;
+    }
+
+    public void setNum1(double num1) {
+        this.num1 = num1;
+    }
+
+    public double getNum2() {
+        return num2;
+    }
+
+    public void setNum2(double num2) {
+        this.num2 = num2;
+    }
+
+    public double obtenerResultado(){
+        if(validar()){
+            return num1 / num2;
+        }
+        return 0;
+    }
+
+    public boolean validar(){
+        if(num2==0){
+            System.out.println("Numero Invalido, no es posible una operacion entre 0");
+            return false;
+        }
+        return true;
+    }
 }
