fbd6b712a1e5de60b0f32d4d9cfa4ef49122a336
Vladimir-28 <20243ds028@utez.edu.mx>
2025-09-02 12:29:01 -0600

    DoWhile

--- src/Main.java
+++ src/Main.java
@@ -13,54 +13,55 @@ System.out.println("4.Dividir");
 System.out.println("5.Salir\n");
 System.out.println("Escoge una opcion");
 int opcion = read.nextInt();
+do {
+    switch (opcion) {
+        case 1:
+            System.out.println("----SUMAR----\n");
+            Sumar adittion = new Sumar();
+            System.out.println("Ingrese el valor del numero 1: ");
+            adittion.setnumber1(read.nextDouble());
+            System.out.println("Ingrese el valor del numero 2: ");
+            adittion.setnumber2(read.nextDouble());
+            System.out.println("resultado: " + adittion.obtenerResultado());
+            break;
+        case 2:
+            System.out.println("----RESTAR----\n");
+            Resta substraction = new Resta();
+            System.out.println("Ingrese el valor del numero 1: ");
+            substraction.setnumber1(read.nextDouble());
+            System.out.println("Ingrese el valor del numero 2: ");
+            substraction.setnumber2(read.nextDouble());
+            System.out.println("resultado: " + substraction.obtenerResultado());
 
-switch(opcion){
-    case 1:
-        System.out.println("----SUMAR----\n");
-        Sumar adittion = new Sumar();
-        System.out.println("Ingrese el valor del numero 1: ");
-        adittion.setnumber1(read.nextDouble());
-        System.out.println("Ingrese el valor del numero 2: ");
-        adittion.setnumber2(read.nextDouble());
-        System.out.println("resultado: " + adittion.obtenerResultado());
-        break;
-    case 2:
-        System.out.println("----RESTAR----\n");
-        Resta substraction = new Resta();
-        System.out.println("Ingrese el valor del numero 1: ");
-        substraction.setnumber1(read.nextDouble());
-        System.out.println("Ingrese el valor del numero 2: ");
-        substraction.setnumber2(read.nextDouble());
-        System.out.println("resultado: " + substraction.obtenerResultado());
+            break;
+        case 3:
+            System.out.println("----MULTIPLICAR----\n");
+            Multiplicar multiplication = new Multiplicar();
+            System.out.println("Ingrese el valor del numero 1: ");
+            multiplication.setnumber1(read.nextDouble());
+            System.out.println("Ingrese el valor del numero 2: ");
+            multiplication.setnumber2(read.nextDouble());
+            System.out.println("resultado: " + multiplication.obtenerResultado());
 
-        break;
-    case 3:
-        System.out.println("----MULTIPLICAR----\n");
-        Multiplicar multiplication = new Multiplicar();
-        System.out.println("Ingrese el valor del numero 1: ");
-        multiplication.setnumber1(read.nextDouble());
-        System.out.println("Ingrese el valor del numero 2: ");
-        multiplication.setnumber2(read.nextDouble());
-        System.out.println("resultado: " + multiplication.obtenerResultado());
+            break;
+        case 4:
+            System.out.println("----DIVIDIR----\n");
+            Dividir divide = new Dividir();
+            System.out.println("Ingrese el valor del numero 1: ");
+            divide.setnumber1(read.nextDouble());
+            System.out.println("Ingrese el valor del numero 2: ");
+            divide.setnumber2(read.nextDouble());
+            System.out.println("resultado: " + divide.obtenerResultado());
+            break;
+        case 5:
+            System.out.println("SALIR....\n");
 
-        break;
-    case 4:
-        System.out.println("----DIVIDIR----\n");
-        Dividir divide = new Dividir();
-        System.out.println("Ingrese el valor del numero 1: ");
-        divide.setnumber1(read.nextDouble());
-        System.out.println("Ingrese el valor del numero 2: ");
-        divide.setnumber2(read.nextDouble());
-        System.out.println("resultado: " + divide.obtenerResultado());
-        break;
-    case 5:
-        System.out.println("SALIR....\n");
-        
-        break;
-    default:
-        System.out.println("Opcion no valida");
-        break;
+            break;
+        default:
+            System.out.println("Opcion no valida");
+            break;
 
     }
+}while(opcion != 5);
 }
 }
